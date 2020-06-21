851478c03922083aefc052d8324aae7452d531b7
Sohail Badghisi <57681550+sohailbadghisi1@users.noreply.github.com>
2020-06-21 21:35:50 +0430

    Update app.js

--- app.js
+++ app.js
@@ -18,56 +18,73 @@ function search(input, cities) {
 }
 
 function displayData() {
-    const ourData = search(input.value, cities);
-    ourData.map(data => {
-        main.innerHTML = `
-        <div class="right">
-            <div class="name">
-                <h4>ولایت ${data.cityName}</h4>
-            </div>
-            <div class='title'>
-
-                <div class='space'>
-                    <span>جمعیت: </span>
-                    <b>‎${data.population}</b>
-                </div>
-           
-                <div class='space'>
-                    <span>مساحت: </span>
-                    <b>${data.area}</b>
-                </div>
-                <div class='space'>
-                    <span>همجوار: </span>
-                    <b>${data.nearCities}</b>
-                </div>
-            
-            </div>
-            <div class='info'>
-             
-                <div class='article'>
-                    <p>
-                        ${data.about}
-                    </p>         
-                </div>
-                
-                <div class='img'>
-                    <img src='${data.img}'>
-                </div>
-                
-            </div>
-        <div class="bottom">
-            <b>ولسوالی ها</b>
-            <span>${data.districts}</span>
-        </div>
-
-      
-
-    </div>
     
+    const ourData = search(input.value, cities);
+    
+    ourData.map(data => {
 
+        if(data.cityName === input.value) {
+          
+            main.innerHTML = `
+            <div class="right">
 
-        `
-    })
+                <div class="name">
+                    <h4>${data.cityName}</h4>
+                </div>
+
+                <div class='title'>
+    
+                    <div class='space'>
+                        <span>جمعیت: </span>
+                        <b>‎${data.population}</b>
+                    </div>
+               
+                    <div class='space'>
+                        <span>مساحت: </span>
+                        <b>${data.area}</b>
+                    </div>
+                    <div class='space'>
+                        <span>همجوار: </span>
+                        <b>${data.nearCities}</b>
+                    </div>
+                
+                </div>
+                <div class='info'>
+                 
+                    <div class='article'>
+                        <p>
+                            ${data.about}
+                        </p>         
+                    </div>
+                    
+                    <div class='img'>
+                        <img src='${data.img}'>
+                    </div>
+                    
+                </div>
+            <div class="bottom">
+                <b>ولسوالی ها</b>
+                <span>${data.districts}</span>
+            </div>
+    
+          
+    
+        </div>
+            `;
+            
+        } else if(input.value === '' ) {
+            main.innerHTML = `
+            <div class="right">
+                <div class="name error">
+                 <h4 class='error-h4'>لطفا نام یک ولایت را وارد کنید</h4>
+                </div>
+            </div>
+            `;
+        }
+       
+    });
+
+    input.value = '';
 }
 
 btn.addEventListener('click', displayData );
